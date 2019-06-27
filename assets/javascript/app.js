b7764f907cbfcab38d36cf262e37cbc3267305d5
abdel <abdelmounaim.lallouache@gmail.com>
2019-06-26 22:57:02 -0500

    first version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,4 +3,10 @@ $(window).scroll(function(){
     $('.navbar').toggleClass('scrolled', $(this).scrollTop() > 50);
     $('.custom-toggler.navbar-toggler').toggleClass('scrolledt', $(this).scrollTop() > 50);
     $('.custom-toggler .navbar-toggler-icon').toggleClass('scrolledti', $(this).scrollTop() > 50);
+    
 });
+
+// $("#project1").on("click",function(){
+   
+//     $("#project1").toggleClass('exp');
+// })
